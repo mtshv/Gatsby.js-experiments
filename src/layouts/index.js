62c9ca3e25d5e42db2272cbaeceab4ff41360ea4
mtshv <i.mityushev@gmail.com>
2018-05-22 21:19:08 +0300

    Programmatically create pages from data

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,43 +1,31 @@
 import React from "react";
 import g from "glamorous";
+import { css } from "glamor";
+import Link from "gatsby-link";
 
 import { rhythm } from "../utils/typography";
 
-export default ({ data }) => {
-    console.log(data);
-    return (
-        <div>
-            <g.H1 display={"inline-block"} borderBottom={"1px solid"}>
-                Amazing Pandas Eating Things
-            </g.H1>
-            <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
-            {data.allMarkdownRemark.edges.map(({ node }) => (
-                <div key={node.id}>
-                    <g.H3 marginBottom={rhythm(1 / 4)}>
-                        {node.frontmatter.title}{" "}
-                        <g.Span color="#BBB">— {node.frontmatter.date}</g.Span>
-                    </g.H3>
-                    <p>{node.excerpt}</p>
-                </div>
-            ))}
-        </div>
-    );
-};
+const linkStyle = css({ float: `right` });
 
-export const query = graphql`
-  query IndexQuery {
-    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
-      totalCount
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            date(formatString: "DD MMMM, YYYY")
-          }
-          excerpt
-        }
-      }
-    }
-  }
-`;
+export default ({ children }) => (
+    <g.Div
+        margin={`0 auto`}
+        maxWidth={700}
+        padding={rhythm(2)}
+        paddingTop={rhythm(1.5)}
+    >
+        <Link to={`/`}>
+            <g.H3
+                marginBottom={rhythm(2)}
+                display={`inline-block`}
+                fontStyle={`normal`}
+            >
+                Pandas Eating Lots
+            </g.H3>
+        </Link>
+        <Link className={linkStyle} to={`/about/`}>
+            About
+        </Link>
+        {children()}
+    </g.Div>
+);
