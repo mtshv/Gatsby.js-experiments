d955d2435e68318f9a0d76e5d655ef8ce3a13f63
mtshv <i.mityushev@gmail.com>
2018-05-22 19:12:28 +0300

    Querying for data in a blog

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,24 +1,40 @@
 import React from "react";
+import g from "glamorous";
+import { css } from "glamor";
 import Link from "gatsby-link";
 
-const ListLink = props =>
-    <li style={{ display: `inline-block`, marginRight: `1rem` }}>
-        <Link to={props.to}>
-            {props.children}
-        </Link>
-    </li>
+import { rhythm } from "../utils/typography";
 
-export default ({ children }) =>
-    <div style={{ margin: `0 auto`, maxWidth: 650, padding: `1.25rem 1rem` }}>
-        <header style={{ marginBottom: `1.5rem` }}>
-            <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-                <h3 style={{ display: `inline` }}>MySweetSite</h3>
-            </Link>
-            <ul style={{ listStyle: `none`, float: `right` }}>
-                <ListLink to="/">Home</ListLink>
-                <ListLink to="/about/">About</ListLink>
-                <ListLink to="/contact/">Contact</ListLink>
-            </ul>
-        </header>
+const linkStyle = css({ float: `right` });
+
+export default ({ children, data }) => (
+    <g.Div
+        margin={`0 auto`}
+        maxWidth={700}
+        padding={rhythm(2)}
+        paddingTop={rhythm(1.5)}
+    >
+        <Link to={`/`}>
+            <g.H3
+                marginBottom={rhythm(2)}
+                display={`inline-block`}
+                fontStyle={`normal`}
+            >
+                {data.site.siteMetadata.title}
+            </g.H3>
+        </Link>
+        <Link className={linkStyle} to={`/about/`}>
+            About
+        </Link>
         {children()}
-    </div>
+    </g.Div>
+);
+
+export const query = graphql`
+  query LayoutQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }`
