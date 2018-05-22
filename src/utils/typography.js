4a13094ebd1a35938a63d782775a65132980a26c
mtshv <i.mityushev@gmail.com>
2018-05-22 18:51:53 +0300

    Building nested layouts in Gatsby

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,6 @@
 import Typography from "typography";
-// import bootstrapTheme from "typography-theme-bootstrap"
-import lawtonTheme from "typography-theme-lawton";
+import fairyGateTheme from "typography-theme-fairy-gates";
 
-const typography = new Typography(lawtonTheme);
+const typography = new Typography(fairyGateTheme);
 
 export default typography;
